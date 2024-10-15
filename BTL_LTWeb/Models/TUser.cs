bd46651ba8b8002bc8a296674cccb87abd68ad10
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-15 22:46:08 +0700

    Update TUser.cs

--- BTL_LTWeb/Models/TUser.cs
+++ BTL_LTWeb/Models/TUser.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace BTL_LTWeb.Models;
 
-public partial class TUser
+public partial class TUser : IdentityUser
 {
     public string Username { get; set; } = null!;
 
     public string Password { get; set; } = null!;
 
     public string? LoaiUser { get; set; }
+    public string Salt { get; set; } = null!;   
 
     public virtual ICollection<TKhachHang> TKhachHangs { get; set; } = new List<TKhachHang>();
 
