94ad4142dbd5cf8e1590ec592628fdef8fca5990
DungZan <142530655+DungZan@users.noreply.github.com>
2024-10-15 23:57:39 +0700

    thêm nhân viên, khách hàng

--- BTL_LTWeb/Models/TKhachHang.cs
+++ BTL_LTWeb/Models/TKhachHang.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 
 namespace BTL_LTWeb.Models;
@@ -18,6 +19,6 @@ public partial class TKhachHang
     public string? DiaChi { get; set; }
 
     public string? GhiChu { get; set; }
-
+    [ValidateNever]
     public virtual TUser UsernameNavigation { get; set; } = null!;
 }
