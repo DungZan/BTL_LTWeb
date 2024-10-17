16b69eb7573d4ee7876343a187dd19a999b3d99f
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-17 14:55:05 +0700

    Create LoginViewModel.cs

---
+++ BTL_LTWeb/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTL_LTWeb.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
