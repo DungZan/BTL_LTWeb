daddacf4e2343079a44380f6356360e8e08b2f00
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-17 14:54:34 +0700

    Update AccountController.cs

--- BTL_LTWeb/Controllers/AccountController.cs
+++ BTL_LTWeb/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using BTL_LTWeb.Models;
+using BTL_LTWeb.ViewModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,7 @@ namespace BTL_LTWeb.Controllers
             _context = context;
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
             if(User.Identity.IsAuthenticated)
@@ -26,17 +28,17 @@ namespace BTL_LTWeb.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(string username, string password, bool rememberMe)
+        public async Task<IActionResult> Login(LoginViewModel login)
         {
-            var user = _context.TUsers.FirstOrDefault(u => u.Username == username);
+            var user = _context.TUsers.FirstOrDefault(u => u.Email == login.Email);
             if (user != null)
             {
-                var hashedPassword = SecurityHelper.HashPasswordWithSalt(password, user.Salt);
+                var hashedPassword = SecurityHelper.HashPasswordWithSalt(login.Password, user.Salt);
                 if (hashedPassword == user.Password)
                 {
                     var claims = new List<Claim>
                     {
-                        new Claim(ClaimTypes.Name, username)
+                        new Claim(ClaimTypes.Email, login.Email)
                     };
 
                     // Tạo ClaimsIdentity cho người dùng
@@ -45,8 +47,8 @@ namespace BTL_LTWeb.Controllers
                     // Thiết lập thuộc tính AuthenticationProperties
                     var authProperties = new AuthenticationProperties
                     {
-                        IsPersistent = rememberMe, // Ghi nhớ đăng nhập nếu "Remember me" được chọn
-                        ExpiresUtc = rememberMe ? DateTime.UtcNow.AddDays(30) : null    // Phiên đăng nhập kéo dài 30 ngày nếu có "Remember me"
+                        IsPersistent = login.RememberMe, // Ghi nhớ đăng nhập nếu "Remember me" được chọn
+                        ExpiresUtc = login.RememberMe ? DateTime.UtcNow.AddDays(30) : DateTime.UtcNow.AddMinutes(30)    // Phiên đăng nhập kéo dài 30 ngày nếu có "Remember me"
                     };
                     Console.WriteLine($"IsPersistent: {authProperties.IsPersistent}, ExpiresUtc: {authProperties.ExpiresUtc}");
                     // Đăng nhập người dùng
@@ -58,14 +60,54 @@ namespace BTL_LTWeb.Controllers
 
             // Thông báo lỗi nếu đăng nhập thất bại
             ModelState.AddModelError(string.Empty, "Tên đăng nhập hoặc mật khẩu không chính xác.");
-            return View("Login");
+            return View(login);
         }
 
+        [HttpPost]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync("MyCookieAuthenticationScheme");
             return RedirectToAction("Login", "Account");
         }
 
+        // register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string username, string password, string confirmPassword)
+        {
+            if (password != confirmPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Mật khẩu không khớp.");
+                return View("Register");
+            }
+
+            var user = _context.TUsers.FirstOrDefault(u => u.Email == username);
+            if (user != null)
+            {
+                ModelState.AddModelError(string.Empty, "Tên đăng nhập đã tồn tại.");
+                return View("Register");
+            }
+
+            var salt = SecurityHelper.GenerateSalt();
+            var hashedPassword = SecurityHelper.HashPasswordWithSalt(password, salt);
+
+            var newUser = new TUser
+            {
+                Email = username,
+                Password = hashedPassword,
+                Salt = salt,
+                LoaiUser = "KhachHang"
+            };
+
+            _context.TUsers.Add(newUser);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login", "Account");
+        }
     }
 }
