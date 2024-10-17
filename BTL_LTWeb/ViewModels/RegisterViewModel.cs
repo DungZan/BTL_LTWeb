fafb6aec4b2e384fb4f0c28b497fd5dad4b51d80
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-18 01:07:18 +0700

    Update RegisterViewModel.cs

--- BTL_LTWeb/ViewModels/RegisterViewModel.cs
+++ BTL_LTWeb/ViewModels/RegisterViewModel.cs
@@ -4,6 +4,22 @@ namespace BTL_LTWeb.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [RegularExpression(@"^[a-zA-Zàáảãạâầấẩẫậăằắẳẵặèéẻẽẹêềếểễệđìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳỵỷỹý -]+$", ErrorMessage = "Tên không hợp lệ")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        [CustomDateValidation(ErrorMessage = "Ngày sinh không hợp lệ.")]
+        public DateOnly? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        [RegularExpression(@"^(0[35789][0-9]{8}|84[35789][0-9]{8}|\+84[35789][0-9]{8})$", ErrorMessage = "Số điện thoại không hợp lệ")]
+        public string PhoneNumber { get; set; }
+
+
         [Required(ErrorMessage = "Email không được để trống")]
         [EmailAddress]
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email không hợp lệ")]
@@ -18,5 +34,32 @@ namespace BTL_LTWeb.ViewModels
 
         [Required(ErrorMessage = "Xác nhận mật khẩu là bắt buộc")]
         public string ConfirmPassword { get; set; }
+    }
+
+    public class CustomDateValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateOnly dateValue)
+            {
+                // Kiểm tra ngày sinh có phải là ngày trong quá khứ không
+                if (dateValue > DateOnly.FromDateTime(DateTime.Now))
+                {
+                    return new ValidationResult("Ngày sinh phải là một ngày trong quá khứ.");
+                }
+
+                // Kiểm tra định dạng của ngày
+                var day = dateValue.Day;
+                var month = dateValue.Month;
+                var year = dateValue.Year;
+
+                // Kiểm tra số ngày trong tháng
+                if (day > DateTime.DaysInMonth(year, month))
+                {
+                    return new ValidationResult("Ngày không hợp lệ cho tháng này.");
+                }
+            }
+            return ValidationResult.Success;
+        }
     }
 }
