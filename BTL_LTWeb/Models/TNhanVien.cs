a68f3619ccd6be96d8233cf8f0df16f740542eb7
DungZan <142530655+DungZan@users.noreply.github.com>
2024-10-21 17:16:35 +0700

    add anotation for DanhmucSP,KhachHang,Nhanvien

--- BTL_LTWeb/Models/TNhanVien.cs
+++ BTL_LTWeb/Models/TNhanVien.cs
@@ -9,15 +9,25 @@ public partial class TNhanVien
 {
     public int MaNhanVien { get; set; }
 
+    [Required(ErrorMessage = "Email không được để trống")]
+    [EmailAddress(ErrorMessage = "Không đúng định dạng")]
     public string Email { get; set; } = null!;
 
+    [Display(Name = "Tên nhân viên")]
+    [Required(ErrorMessage = "Tên nhân viên không được để trống")]
     public string? TenNhanVien { get; set; }
 
+    [DataType(DataType.Date)]
+    [Display(Name = "Ngày sinh")]
     public DateOnly? NgaySinh { get; set; }
 
+    [Display(Name = "Chức vụ")]
+    [Required(ErrorMessage = "Chức vụ không được để trống")]
     public string? ChucVu { get; set; }
 
+    [Display(Name = "Ghi chú")]
     public string? GhiChu { get; set; }
+
     [ValidateNever]
     public virtual TUser UsernameNavigation { get; set; } = null!;
 }
