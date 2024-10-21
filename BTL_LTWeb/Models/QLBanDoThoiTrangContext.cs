c986d65eb7a75b96daff4e2b08ffcbace4078923
Trương Văn Minh <truongvan.minh1504@gmail.com>
2024-10-21 16:40:38 +0700

    Update QLBanDoThoiTrangContext.cs

--- BTL_LTWeb/Models/QLBanDoThoiTrangContext.cs
+++ BTL_LTWeb/Models/QLBanDoThoiTrangContext.cs
@@ -31,7 +31,7 @@ public partial class QLBanDoThoiTrangContext : DbContext
     public virtual DbSet<TDanhSachCuaHang> TDanhSachCuaHangs { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-PIULBJ0\\SQLEXPRESS01;Initial Catalog=QLBanDoThoiTrang;Integrated Security=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=QLBanDoThoiTrang;Integrated Security=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
