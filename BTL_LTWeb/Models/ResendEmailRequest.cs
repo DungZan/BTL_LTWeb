6e06ea60f3ce00c9482ec40e7e2a0edfa90c5f32
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-23 20:47:56 +0700

    Create ResendEmailRequest.cs

---
+++ BTL_LTWeb/Models/ResendEmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BTL_LTWeb.Models
+{
+    public class ResendEmailRequest
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Status { get; set; }
+    }
+}
