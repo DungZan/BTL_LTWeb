d52063789d61b81bf8c2aa2e1bf770503a5493b4
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-24 16:13:01 +0700

    Delete ResendEmailRequest.cs

--- BTL_LTWeb/Models/ResendEmailRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BTL_LTWeb.Models
-{
-    public class ResendEmailRequest
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public int Status { get; set; }
-    }
-}
