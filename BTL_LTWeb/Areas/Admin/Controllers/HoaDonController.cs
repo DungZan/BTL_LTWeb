f70d16805f40ba8b152dc79434b6c9d1e9fb9b2f
DungZan <142530655+DungZan@users.noreply.github.com>
2024-10-27 10:59:04 +0700

    add controller

---
+++ BTL_LTWeb/Areas/Admin/Controllers/HoaDonController.cs
@@ -0,0 +1,24 @@
+﻿using BTL_LTWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace BTL_LTWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/HoaDon")]
+    public class HoaDonController : Controller
+    {
+        QLBanDoThoiTrangContext db = new QLBanDoThoiTrangContext();
+        //hoá đơn bán
+        [Route("danhsachhoadon")]
+        public IActionResult danhsachhoadon(int? Page)
+        {
+            int pageSize = 10;
+            int pageNumber = Page == null || Page <= 0 ? 1 : Page.Value;
+            var list = db.THoaDonBans.Include(m => m.NhanVien).Include(m => m.KhachHang);
+            PagedList<THoaDonBan> lst = new PagedList<THoaDonBan>(list, pageNumber, pageSize);
+            return View(lst);
+        }
+    }
+}
