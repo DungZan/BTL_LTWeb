3a188d2c9ce5ce3fb16df43603e8ac80fef9abb3
NullnMoon <tmquang031104@gmail.com>
2024-10-31 18:14:41 +0700

    test

--- BTL_LTWeb/Controllers/PayController.cs
+++ BTL_LTWeb/Controllers/PayController.cs
@@ -74,6 +74,57 @@ namespace BTL_LTWeb.Controllers
         }
 
         [HttpPost]
+        public IActionResult CheckDiscountCode([FromForm] CheckoutViewModel checkoutViewModel)
+        {
+
+            
+            var discount = _context.TMaGiamGias.FirstOrDefault(e => e.Code == checkoutViewModel.DiscountCode);
+            Console.WriteLine(discount);
+            if (discount == null || !discount.TrangThai.HasValue || discount.TrangThai == 0)
+            {
+                return Json(new { success = false, message = "Mã giảm giá không tồn tại hoặc đã hết hạn" });
+            }
+            var isUsed = _context.TMaGiamGiaDaSuDungs.Any(x => x.MaGiamGia == discount.MaGiamGia && x.MaKhachHang == checkoutViewModel.CustomerInfo.MaKhachHang );
+            if (isUsed)
+            {
+                return Json(new { success = false, message = "Mã giảm  giá đã được sử dụng" });
+            }
+
+            decimal totalDiscount = 0;
+            if (discount.LoaiGiamGia == 1)
+            {
+                decimal totalCartAmount = checkoutViewModel.CartItems.Sum(item =>
+                {
+                    var productPrice = _context.TDanhMucSps.Where(d => d.MaSp == _context.TChiTietSanPhams.Where(sp => sp.MaChiTietSp == item.MaChiTietSP).Select(sp => sp.MaSp).FirstOrDefault()).Select(d => d.Gia).FirstOrDefault() ?? 0;
+                    return item.SoLuong * productPrice;
+                });
+                totalDiscount = discount.TiLeGiam * totalCartAmount;
+            }
+            else if (discount.LoaiGiamGia == 2)
+            {
+                var applicableProductIds = _context.TMaGiamGiaSanPhams.Where(e => e.MaGiamGia == discount.MaGiamGia).Select(e => e.MaSp).ToList();
+                var applicableItems = checkoutViewModel.CartItems.Select(item => new
+                {
+                    item.MaChiTietSP,
+                    MaSp = _context.TChiTietSanPhams.Where(sp => sp.MaChiTietSp == item.MaChiTietSP).Select(sp => sp.MaSp).FirstOrDefault()
+                }).Where(item => item.MaSp != null && applicableProductIds.Contains(item.MaSp)).ToList();
+
+                if (!applicableItems.Any())
+                {
+                    return Json(new { success = false, message = "Mã giảm giá không áp dụng cho sản phẩm trong giỏ hàng!" });
+                }
+                foreach (var item in applicableItems)
+                {
+                    var productPrice = _context.TDanhMucSps.Where(d => d.MaSp == item.MaSp).Select(d => d.Gia).FirstOrDefault() ?? 0;
+                    var itemCount = checkoutViewModel.CartItems.FirstOrDefault(x => x.MaChiTietSP == item.MaChiTietSP)?.SoLuong ?? 1;
+                    totalDiscount += discount.TiLeGiam * productPrice * itemCount;
+                }
+            }
+
+            return Json(new { success = true, totalDiscount });
+        }
+
+        [HttpPost]
         public IActionResult ProcessPayment([FromForm] PaymentViewModel model)
         {
             if (model == null)
