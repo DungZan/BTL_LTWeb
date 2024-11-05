c5aba190b6a39b9598c10add688077ae7745af4a
Mr-1504 <truongvan.minh1504@gmail.com>
2024-11-06 00:07:38 +0700

    Update RoleCheckMiddleware.cs

--- BTL_LTWeb/Middlewares/RoleCheckMiddleware.cs
+++ BTL_LTWeb/Middlewares/RoleCheckMiddleware.cs
@@ -11,19 +11,14 @@
 
         public async Task Invoke(HttpContext context)
         {
-            // Kiểm tra nếu người dùng đã đăng nhập
             if (context.User.Identity.IsAuthenticated)
             {
-                // Kiểm tra session để xem người dùng đã được điều hướng hay chưa
                 if (context.Session.GetString("HasRedirected") != "true")
                 {
-                    // Đánh dấu rằng đã điều hướng
                     context.Session.SetString("HasRedirected", "true");
 
-                    // Kiểm tra vai trò của người dùng
                     if (context.User.IsInRole("KhachHang"))
                     {
-                        // Nếu không ở trang Home thì điều hướng đến trang Home
                         if (!context.Request.Path.StartsWithSegments("/Home/Home"))
                         {
                             context.Response.Redirect("/Home/Home");
@@ -32,7 +27,6 @@
                     }
                     else
                     {
-                        // Nếu không ở trang Admin thì điều hướng đến trang Admin
                         if (!context.Request.Path.StartsWithSegments("/Admin/Index"))
                         {
                             context.Response.Redirect("/Admin/Index");
@@ -41,8 +35,6 @@
                     }
                 }
             }
-
-            // Tiếp tục xử lý request
             await _next(context);
         }
     }
